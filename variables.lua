29092c3e43b747d7aa43b8d71c19a17f1ff484ef
Pablo Gonzalez Santatecla <paos@gmv.es>
2024-03-07 09:26:04 +0100

    Variables

--- variables.lua
+++ variables.lua
@@ -1,10 +1,22 @@
-a = 29
-b = 28
+local d, f= 5, 10 -- declaration of d and f as local variables
+d, f = 5, 10 -- declaration of d and f as global variables
+d, f = 10 -- declaration of d and f as global variables, but f is nil
 
-print(a)
-print(b)
+-- Variable definition
+local a, b
 
+-- Initialization
+a = 10
+b = 30
+
+print("Value of a:", a)
+print("Value of b:", b)
+
+-- Swapping
 b, a = a, b
 
-print(a)
-print(b)
+print("Value of a:", a)
+print("Value of b:", b)
+
+f = 70.0/3.0
+print("Value of f:", f)
