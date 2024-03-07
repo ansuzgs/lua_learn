f9d3539ba20910a1c7efdf2d106d3c47cd3d950c
Pablo Gonzalez Santatecla <paos@gmv.es>
2024-03-07 10:32:04 +0100

    Operators

---
+++ operators.lua
@@ -0,0 +1,33 @@
+--[[
+    OPERATORS
+]]
+
+-- Arithmetic operators
+
+print("5 + 2 = ", 5+2)
+print("5 - 2 = ", 5-2)
+print("5 * 2 = ", 5*2)
+print("5 / 2 = ", 5/2) 
+print("5 % 2 = ", 5%2)
+print("5 ^ 2 = ", 5^2)
+print("-5 = ", -5)
+
+-- Relational operators
+
+print("5 == 2 is ", 5==2)
+print("5 ~= 2 is ", 5~=2)
+print("5 > 2 is ", 5>2)
+print("5 < 2 is ", 5<2)
+print("5 >= 2 is ", 5>=2)
+print("5 <= 2 is ", 5<=2)
+
+-- Logical operators
+
+print("true and false is ", true and false)
+print("true or false is ", true or false)
+print("not true is ", not true)
+
+-- Other operators
+
+print("Hello" .. "World")
+print("#'Hello' is ", #'Hello')
